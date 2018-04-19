b0fa490f1df33d00978db70a7844971f6d5079bd
Colin Russell <colinrussell101@gmail.com>
2018-04-19 17:04:17 -0400

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by Colin on 2018-04-19.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
